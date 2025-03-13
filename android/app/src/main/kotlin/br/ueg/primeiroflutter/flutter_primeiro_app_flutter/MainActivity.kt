570243e587cfba88b7e5e91d96b4c319c0525908
Isaque <crisperohman@gmail.com>
2025-03-12 22:01:23 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/br/ueg/primeiroflutter/flutter_primeiro_app_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.ueg.primeiroflutter.flutter_primeiro_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+// import 'package:signals/signals_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'memo.io',
+              style: TextStyle(fontSize: 36),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
